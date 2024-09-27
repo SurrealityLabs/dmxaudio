41fcb4876d7455b869a712a9dc06399ab671eb1c
Randy Glenn <randy@surrealitylabs.com>
2024-09-27 00:16:42 -0400

    Adding more tests

---
+++ test-scripts/test16.py
@@ -0,0 +1,148 @@
+#!/usr/bin/python
+
+from dmx import Colour, DMXInterface, DMXLight3Slot, DMXUniverse
+import testCommands
+import time
+
+# Open an interface
+with DMXInterface("FT232R") as interface:
+    # Create a universe
+    universe = DMXUniverse()
+
+    # Define a light
+    lightA = DMXLight3Slot(address=0)
+    lightB = DMXLight3Slot(address=3)
+
+    # Add the light to a universe
+    universe.add_light(lightA)
+    universe.add_light(lightB)
+
+    # Update the interface's frame to be the universe's current state
+    interface.set_frame(universe.serialise())
+
+    # Send an update to the DMX network
+    interface.send_update()
+
+    # Play the sound
+    lightA.set_colour(testCommands.PlaySound256LoopPolyA)
+    lightB.set_colour(testCommands.PlaySound256LoopPolyB)
+
+    print("Playing sound 256, looping, moderate volume - did you hear it?")
+
+    # Update the interface's frame to be the universe's current state
+    interface.set_frame(universe.serialise())
+
+    # Send an update to the DMX network
+    interface.send_update()
+
+    time.sleep(5)
+
+    # Play the sound
+    lightA.set_colour(testCommands.PlaySound257LoopPolyA)
+    lightB.set_colour(testCommands.PlaySound257LoopPolyB)
+
+    print("Playing sound 257, looping, moderate volume - did you hear it?")
+
+    # Update the interface's frame to be the universe's current state
+    interface.set_frame(universe.serialise())
+
+    # Send an update to the DMX network
+    interface.send_update()
+
+    time.sleep(5)
+
+    # Play the sound
+    lightA.set_colour(testCommands.PlaySound258LoopPolyA)
+    lightB.set_colour(testCommands.PlaySound258LoopPolyB)
+
+    print("Playing sound 258, looping, moderate volume - did you hear it?")
+
+    # Update the interface's frame to be the universe's current state
+    interface.set_frame(universe.serialise())
+
+    # Send an update to the DMX network
+    interface.send_update()
+
+    time.sleep(5)
+
+    # Play the sound
+    lightA.set_colour(testCommands.PlaySound259LoopPolyA)
+    lightB.set_colour(testCommands.PlaySound259LoopPolyB)
+
+    print("Playing sound 259, looping, moderate volume - did you hear it?")
+
+    # Update the interface's frame to be the universe's current state
+    interface.set_frame(universe.serialise())
+
+    # Send an update to the DMX network
+    interface.send_update()
+
+    time.sleep(5)
+
+    # Play the sound
+    lightA.set_colour(testCommands.PlaySound260LoopOnceA)
+    lightB.set_colour(testCommands.PlaySound260LoopOnceB)
+
+    print("Playing sound 260, looping, moderate volume - did you hear it?")
+
+    # Update the interface's frame to be the universe's current state
+    interface.set_frame(universe.serialise())
+
+    # Send an update to the DMX network
+    interface.send_update()
+
+    time.sleep(0.5)
+
+    # Play the sound
+    lightA.set_colour(testCommands.PlaySound261LoopOnceA)
+    lightB.set_colour(testCommands.PlaySound261LoopOnceB)
+
+    print("Playing sound 261, looping, moderate volume - did you hear it?")
+
+    # Update the interface's frame to be the universe's current state
+    interface.set_frame(universe.serialise())
+
+    # Send an update to the DMX network
+    interface.send_update()
+
+    time.sleep(0.5)
+
+    # Play the sound
+    lightA.set_colour(testCommands.PlaySound262LoopOnceA)
+    lightB.set_colour(testCommands.PlaySound262LoopOnceB)
+
+    print("Playing sound 262, looping, moderate volume - did you hear it?")
+
+    # Update the interface's frame to be the universe's current state
+    interface.set_frame(universe.serialise())
+
+    # Send an update to the DMX network
+    interface.send_update()
+
+    time.sleep(0.5)
+
+    # Play the sound
+    lightA.set_colour(testCommands.PlaySound263LoopOnceA)
+    lightB.set_colour(testCommands.PlaySound263LoopOnceB)
+
+    print("Playing sound 263, looping, moderate volume - did you hear it?")
+
+    # Update the interface's frame to be the universe's current state
+    interface.set_frame(universe.serialise())
+
+    # Send an update to the DMX network
+    interface.send_update()
+
+    time.sleep(5)
+
+    # Send a Stop All
+    lightA.set_colour(StopAllCommandA)
+    lightB.set_colour(StopAllCommandB)
+
+    print("Sending a Stop All, sound should stop")
+
+    # Update the interface's frame to be the universe's current state
+    interface.set_frame(universe.serialise())
+
+    # Send an update to the DMX network
+    interface.send_update()
